830882ebb4427a7e4ad360da628b3b7944d91690
Chuntak <Chuntak.kong@stonybrook.edu>
2016-12-11 05:31:24 -0500

    Hi my name is Chucky.

--- client.py
+++ client.py
@@ -36,89 +36,95 @@ def client():
 		
 		for sock in read:
 			#message from server
-                        #receive from server
+            #receive from server
 			if sock == s:
 				data = sock.recv(4096)
 				if not data:
 					print("Not connected to Server")
 					sys.exit()
 				else:
-					#print server message
-					sys.stdout.write(data +'\n')
-					
-
-					sys.stdout.flush()
-					
-			#receive from server
+					dataList = data.split("\a.\a.") 
+					sys.stdout.write(dataList[0] +'\n')
+					sys.stdout.flush()		
+					if(len(dataList) > 1):
+						if(dataList[1] == "end"):
+							set_mode("DEFAULT")
 			else:
 				#send message to server
 				msg = sys.stdin.readline()
 				msg_parse = msg.strip().split()
-				if msg.strip() == "logout":
-                                        s.send(msg)
-					sys.exit()
+				if(len(msg_parse) > 0):
+					if msg.strip() == "logout":
+						s.send(msg)
+						sys.exit()
+					elif msg_parse[0] == "login":
+						s.send(msg)
+					#command mode AG	
+					elif(msg_parse[0]=="ag"):
+						set_mode("ag") 
+						s.send(msg)
+											
+					#command mode SG                
+					elif(msg_parse[0]=="sg"):
+						set_mode("sg")
+						s.send(msg)
 
-				#command mode AG	
-				elif(msg_parse[0]=="ag"):
-                                        set_mode("ag") 
-                                        s.send(msg)
-                                        
-                                #command mode SG                
-                                elif(msg_parse[0]=="sg"):
-                                        set_mode("sg")
-                                        s.send(msg)
+					#command mode RG        
+					elif(msg_parse[0]=="rg"):
+						set_mode("rg")
+						s.send(msg)        
+							
+					elif(msg_parse[0]=="s"):
+						if(MODE=="sg"):
+							arg_str = "sg " + msg
+							s.send(arg_str)
+						elif(MODE=="ag"):
+							arg_str = "ag " + msg
+							s.send(arg_str)
+						else:
+							print("Invalid sub-command for mode " + MODE)  
+					elif(msg_parse[0]=="u"):
+						if(MODE=="sg"):
+							arg_str = "sg " + msg
+							s.send(arg_str)
+						elif(MODE=="ag"):
+							arg_str = "ag " + msg
+							s.send(arg_str)
+						else:
+							print("Invalid sub-command, no mode was specified")
+									
+					elif(msg_parse[0]=="p" or msg_parse[0]=="r"):
+						if(MODE=="rg"):
+							arg_str = "rg " + msg
+							send(arg_str)
+						else:
+							print("Invalid sub-command for mode " + MODE)
+					elif(msg_parse[0]=="q"):
+						set_mode("DEFAULT")
 
-                                #command mode RG        
-                                elif(msg_parse[0]=="rg"):
-                                        set_mode("rg")
-                                        s.send(msg)        
-                                        
-                                elif(msg_parse[0]=="s"):
-                                        if(MODE=="sg"):
-                                                arg_str = "sg " + msg
-                                                s.send(arg_str)
-                                        else:
-                                              print("Invalid sub-command for mode " + MODE)  
-                                elif(msg_parse[0]=="u"):
-                                        if(MODE=="sg"):
-                                                arg_str = "sg " + msg
-                                                s.send(arg_str)
-                                        elif(MODE=="ag"):
-                                                arg_str = "ag " + msg
-                                                s.send(arg_str)
-                                        else:
-                                                print("Invalid sub-command, no mode was specified")
-                                                
-                                elif(msg_parse[0]=="p" || msg_parse[0]=="r"):
-                                        if(MODE=="rg"):
-                                                arg_str = "rg " + msg
-                                                send(arg_str)
-                                        else:
-                                                print("Invalid sub-command for mode " + MODE)
-                                elif(msg_parse[0]=="q"):
-                                        set_mode("DEFAULT")
-
-                                elif(msg_parse[0]=="n"):
-                                        if(MODE=="sg"):
-                                                arg_str = "sg " + msg
-                                                s.send(arg_str)
-                                        elif(MODE=="ag"):
-                                                arg_str = "ag " + msg
-                                                s.send(arg_str)
-                                        elif(MODE=="rg"):
-                                                arg_str = "rg " + msg
-                                                s.send(arg_str)
-
-                                        else:
-                                                 print("Invalid sub-command, no mode was specified")
-                                        
-                                     
+					elif(msg_parse[0]=="n"):
+						if(MODE=="sg"):
+							arg_str = "sg " + msg
+							s.send(arg_str)
+						elif(MODE=="ag"):
+							arg_str = "ag " + msg
+							s.send(arg_str)
+						elif(MODE=="rg"):
+							arg_str = "rg " + msg
+							s.send(arg_str)
+						else:
+							print(msg_parse)
+							print(MODE)
+							print("Invalid sub-command, no mode was specified")            
+					elif(msg.strip() == "help"):
+						s.send("help")
                 
 
 
 #set the current mode
 def set_mode(string):
-        MODE = string
+	global MODE
+	MODE = string
         
 if __name__ == "__main__":
 	sys.exit(client())
--- server.py
+++ server.py
@@ -19,19 +19,41 @@ GROUP_LIST = []
 user_list = []
 global_ID = 0
 
-help_menu = "MAKE HELP MENU"
+help_menu = "SEND HELP SUSAN!!!! HALP"
+
+conn = sqlite3.connect('./database/InterestDB.db')
+conn.row_factory = sqlite3.Row
+connCursor = conn.cursor()
 
 class User:
-	def __init__(self, username, ID, socket):
+	def __init__(self, username, socket):
 		self.username = username
-		self.ID = ID
 		self.socket = socket
+		self.groupList = []
+		self.numLineToRead = 0
+		self.groupIndex = 0
+	def reset(self):
+		self.groupList = []
+		self.numLineToRead = 0
+		self.groupIndex = 0
 
 class Group:
-        def __init__(self, groupname, groupID, isSubscribed):
-                self.groupname = groupname
-                self.groupID = groupID
-                self.isSubscribed = isSubscribed
+	def __init__(self, groupname, groupID):
+		self.groupname = groupname
+		self.groupID = groupID
+		self.isSubscribed = False
+		self.posts = []
+	
+class Posts:
+	def __init__(self, subject, username, weekDay, datetime, timezoneYear, post, groupId):
+		self.subject = subject
+		self.username = username
+		self.weekDay = weekDay
+		self.datetime = datetime
+		self.timezoneYear = timezoneYear
+		self.post = post
+		self.groupId = groupId
+		self.new = True
 
 def server():
 	#establish server socket
@@ -58,185 +80,247 @@ def server():
 		#received from client
 			else:
 				#process client data
-				try:
-					#data from socket
-					data = sock.recv(BUFF)
-					if data:
-						#deal with data
-						print(str(data))
-						data_list = data.strip().split()
-						
-						#help menu
-						if data.strip() == "help":
+				#data from socket
+				data = sock.recv(BUFF)
+				if data:
+					#deal with data
+					print(str(data))
+					data_list = data.strip().split()
+					
+					#help menu
+					if data.strip() == "help":
+						sock.send(help_menu)
+					elif data_list[0] == "login":
+						if(getUserBySocket(sock) is not None):
+							o=0 #LAZY WAY TO IGNORE A CONDITION
+						elif len(data_list) < 2:
 							sock.send(help_menu)
-							
-						elif data_list[0] == "login":
-							if len(data_list) < 2:
-								sock.send(help_menu)
+						else:
+							user = loginUser(data_list[1], sock)
+							user_list.append(user) 
+							print(data_list[1] + " has logged in")
+							sock.send(data_list[1] + " has logged in")
+					#arg command
+					elif data_list[0] == "ag":
+						if len(data_list)<2:
+							get_groups(DEFAULT, sock)
+						else:
+							if(data_list[1] == "s"):
+								subscribeGroups(data_list, sock)
+							elif(data_list[1] == "u"):
+								unsubscribeGroups(data_list, sock)
+							elif(data_list[1] == "n"):
+								print_restGroups(sock, "ag")
+							elif(data_list[1] == "q"):
+								set_mode("DEFAULT")
+							elif(data_list[1].isdigit()):
+								get_groups(int(data_list[1]), sock)
 							else:
-								global global_ID #access global variables
-								user = User(data_list[1], 0, sock)
-								global_ID = global_ID + 1
-								user_list.append(user) 
-								print(data_list[1] + " has logged in")
-								sock.send(data_list[1] + " has logged in")
-
-						#arg command
-						elif data_list[0] == "ag":
-                                                        if len(data_list)<2:
-                                                                get_group(DEFAULT)
-                                                        else:
-                                                                if(data_list[1] == "s"):
-                                                                        subscribeGroup(data_list)
-                                                                elif(data_list[1] == "u"):
-                                                                        unsubscribeGroup(data_list)
-                                                                elif(data_list[1] == "n"):
-                                                                        print_restGroups()
-                                                                elif(data_list[1] == "q"):
-                                                                
-                                                                else:
-                                                                        sock.send(help_menu)
-                                                                get_group(data_list[1])
-                                                #sg command
-                                                elif data_list[0] == "sg":
-                                                        if len(data_list)<2:
-                                                                get_subscribeGroups(DEFAULT)
-                                                        else:
-                                                                set_mode("sg")
-                                                                get_subscribeGroups(data_list[1])
-
-                                                #check for mode and specific sub_command for each mode
-                                                elif data_list[0] == "s" || data_list[0] == "u"
-                                                        || data_list[0] == "n" || data_list[0] == "q":
-                                                        if(MODE=="ag"):
-                                                                if(data_list[0] == "s"):
-                                                                        subscribeGroup(data_list)
-                                                                elif(data_list[0] == "u"):
-                                                                        unsubscribeGroup(data_list)
-                                                                elif(data_list[0] == "n"):
-                                                                        print_restGroups()
-                                                                elif(data_list[0] == "q"):
-                                                                        set_mode("DEFAULT")
-                                                                else:
-                                                                        sock.send(help_menu)
-                                                        elif(MODE=="sg"):
-                                                                elif(data_list[0] == "u"):
-                                                                        unsubscribeGroup(data_list)
-                                                                elif(data_list[0] == "n"):
-                                                                        print_restGroups()
-                                                                elif(data_list[0] == "q"):
-                                                                        set_mode("DEFAULT")
-                                                                else:
-                                                                        sock.send(help_menu)        
-                                                        else:
-                                                                sock.send(help_menu)
-                                                                
-                                                #logout
-						elif data.strip() == "logout":
-							sock.send("Bye")
-							LIST_SOCKETS.remove(sock)
-							for i in user_list:
-								if i.socket == sock:
-									user_list.remove(i)
+								sock.send(help_menu)
+					#sg command
+					elif data_list[0] == "sg":
+						if len(data_list)<2:
+							get_subscribeGroups(DEFAULT, sock)
+						else:
+							if(data_list[1] == "n"):
+								print_restGroups(sock, "sg")
+							elif(data_list[1] == "u"):
+								unsubscribeGroups(data_list, sock)
+							elif(data_list[1] == "q"):
+								set_mode("DEFAULT")
+							elif(data_list[1].isdigit()):
+								get_subscribeGroups(int(data_list[1]), sock)
+							else:
+								sock.send(help_menu)
+					elif data.strip() == "logout":
+						sock.send("Bye")
+						LIST_SOCKETS.remove(sock)
+						for i in user_list:
+							if i.socket == sock:
+								user_list.remove(i)
 					else:
 						#remove socket
 						if sock in LIST_SOCKETS:
 							LIST_SOCKETS.remove(sock)
-				except Exception as e: 
-					#send out
-					print("DAVE YOU IDIOT")
-					print str(e)
-					continue
 
 	server_socket.close()
 	
 # For command AG
 # @para N number of groups to print or default value
-def get_groups(N):
-        MODE = "ag"
-        #for loop
-                print_result_AG_SG()
+def loginUser(username, socket):
+	usersDB = accessSQL("SELECT * FROM users")
+	for u in usersDB:
+		if(u['userId'] == username):
+			return User(username, socket)
+	accessSQL("INSERT INTO users VALUES (\'" + username + "\')")
+	print("HERE")
+	return User(username, socket)
 
+def accessSQL(statement):
+	connCursor.execute(statement)
+	dbreturned = connCursor.fetchall()
+	conn.commit()
+	return dbreturned
 
+def getUserBySocket(sock):
+	for u in user_list:
+		if(u.socket == sock):
+			return u
+	return None
+	
+def getUserByUserName(userName):
+	for u in user_list:
+		if(u.socket == userName):
+			return u
+	return None
+	
+def get_groups(N, sock):
+	#for loop	
+	groupsDB = accessSQL("SELECT * FROM groups")
+	user = getUserBySocket(sock)	
+	subscriptions = accessSQL("SELECT * FROM subscription")
+	user.reset()
+	for g in groupsDB:
+		groupModel = Group(g['groupName'], g['groupID'])
+		user.groupList.append(groupModel)
+		for s in subscriptions:
+			if(s['groupID'] == g['groupID'] and s['userId'] == user.username):
+				groupModel.isSubscribed = True		
+	user.numLineToRead = N
+	sendGroup(user,user.groupIndex, user.groupIndex + N, sock, "ag")
+	
+	
+def get_subscribeGroups(N, sock):
+	user = getUserBySocket(sock)
+	query = "SELECT * FROM groups, subscription WHERE subscription.userId == \'%s\' and groups.groupID == subscription.groupID" % (user.username)
+	groupsDB = accessSQL(query)
+	user.reset()
+	for g in groupsDB:
+		groupModel = Group(g['groupName'], g['groupID'])
+		user.groupList.append(groupModel)
+		groupModel.isSubscribed = True
+	user.numLineToRead = N
+	sendGroup(user,user.groupIndex, user.groupIndex + N, sock, "sg")
+
+def sendGroup(user ,start, end, sock, mode):
+	s = ""
+	for x in range(start, end):
+		if(x >= len(user.groupList)):
+			break
+		if(mode == "ag"):
+			if(user.groupList[x].isSubscribed):
+				s = s + str(x+1) + ". (S) " + user.groupList[x].groupname + "\n"
+			else:
+				s = s + str(x+1) + ". ( ) " + user.groupList[x].groupname + "\n"
+		elif(mode == "sg"):
+			npNum = getNumNewPost(user.groupList[x], user)
+			s = s + str(x+1) + ".\t" + str(npNum) + "\t" + user.groupList[x].groupname + "\n"
+		user.groupIndex += 1
+	sock.send(s)
+	if(x >= len(user.groupList)):
+		sock.send("\a.\a.end")
+		
+#ONLY WORKS IF THE GROUP IS IN THE USER LIST
+def getNumNewPost(group, user):
+	getGroupPosts(group)
+	query = "SELECT * FROM postStatus"
+	readPost = accessSQL(query)
+	x = len(group.posts)
+	for p in group.posts:
+		for rp in readPost:
+			if(rp['groupId'] == p.groupId and rp['subject'] == p.subject):
+				p.new = False
+				x -= 1
+	return x
+	
+
+def getGroupPosts(group):
+	query = "SELECT * FROM posts WHERE posts.groupId = %d" % group.groupID
+	postsDB = accessSQL(query)
+	for p in postsDB:  
+		postModel = Posts(p['subject'], p['userId'], p['weekDay'], p['date_time'], p['timeZone_year'], p['post'], p['groupId'])
+		group.posts.append(postModel)
+	
+	
 # For command AG or SG
 # @para N number of groups to print or default value
-def subscribeGroups(data_list):
-        if len(data_list)<2:
-                sock.send(help_menu)
-        elif:
-                for a in range(1, len(data_list))
-                        #subcribe here
-
-        #reset counter
-        return True
+def subscribeGroups(data_list, sock):
+	if len(data_list)<3:
+		sock.send(help_menu)
+	else:
+		for x in range(2, len(data_list)):
+			user = getUserBySocket(sock)
+			subscribedGroup = user.groupList[int(data_list[x]) - 1]
+			accessSQL("INSERT INTO subscription VALUES (\'" + str(subscribedGroup.groupID)  + "\',\'" + user.username + "\')")
+	#reset counter
+	return True
 
 #For command AG or SG
-def unsubscribeGroups(data_list):
-        final_str = ""
-        if len(data_list)<2:
-                sock.send(help_menu)
-        else:
-                for a in range(1, len(data_list))
-                        #unsubscribe here
-                
+def unsubscribeGroups(data_list, sock):
+	if len(data_list)<3:
+		sock.send(help_menu)
+	else:
+		for x in range(2, len(data_list)):
+			user = getUserBySocket(sock)
+			subscribedGroup = user.groupList[int(data_list[x]) - 1]		
+			query = "DELETE FROM subscription WHERE groupID = %d and userId = \'%s\'" % (subscribedGroup.groupID, user.username)
+			accessSQL(query)
+	return True
 
-        #reset counter
-        return True
+def print_restGroups(sock, mode):
+	user = getUserBySocket(sock)
+	print(user.groupIndex)
+	sendGroup(user, user.groupIndex, user.groupIndex + user.numLineToRead, sock, mode)
+	#reset counter
+	return True
+def print_post(sock):
+	final_str = "" 
+	final_str = final_str + "Group: " + "put" + "\n"
+	final_str = final_str + "Subject: " + "put" + "\n"
+	final_str = final_str + "Author: " + "put" + "\n"
+	final_str = final_str + "Date: " + "put" + "\n"
 
-def print_restGroups():
-        if len(data_list)<2:
-                sock.send(help_menu)
-        elif
-
-        #reset counter
-        return True
-def print_post():
-        final_str = "" 
-        final_str = final_str + "Group: " + "put" + "\n"
-        final_str = final_str + "Subject: " + "put" + "\n"
-        final_str = final_str + "Author: " + "put" + "\n"
-        final_str = final_str + "Date: " + "put" + "\n"
-
-        sock.send(final_str)
+	sock.send(final_str)
 
 #Prints for commands AG and SG
-def print_result_AG_SG(final_str, data_list):
+def print_result_AG_SG(final_str, data_list, sock):
 
-        #print group counter 
-        print(counter + ". ")
+	#print group counter 
+	print(counter + ". ")
 
-        if(MODE=="ag"):
-                #check if the current user is subscribed to this specific group
-                if():
-                        print("(" + "put status here" + ")  ")
-                else:
-                        print("( )")
-        elif(MODE=="sg"):
-                #print the number of unread/new posts or empty string
-                print("put number of unread/new post here" + "  ")
-                
-        #print the group name
-        print("put here")
+	if(MODE=="ag"):
+			#check if the current user is subscribed to this specific group
+		if():
+			print("(" + "put status here" + ")  ")
+		else:
+			print("( )")
+	elif(MODE=="sg"):
+			#print the number of unread/new posts or empty string
+		print("put number of unread/new post here" + "  ")
+			
+	#print the group name
+	print("put here")
 
 #Prints for command RG
 #Order them by unread/new posts first
-def print_resultRG():        
-        #print group counter 
-        print(counter + ". ")
+def print_resultRG(sock):        
+	#print group counter 
+	print(counter + ". ")
 
-        #status of the post, "N" for unread/new post or empty string for read
-        print(" put N or empty space" + " ")
+	#status of the post, "N" for unread/new post or empty string for read
+	print(" put N or empty space" + " ")
 
-        #date and time of the post
-        print("put date/time here" + " ")
+	#date and time of the post
+	print("put date/time here" + " ")
 
-        #sibject of the post
-        print("put subject of post here")
+	#sibject of the post
+	print("put subject of post here")
 
 #set the cu
 def set_mode(string):
-        MODE = string
-        
+	MODE = string
+	
 
 if __name__ == "__main__":
-	sys.exit(server())
+	server()
+	sys.exit()
